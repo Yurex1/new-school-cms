9c036f955da468e7bd72d623373eb20b797f4149
yurex1 <103779514+Yurex1@users.noreply.github.com>
2023-05-25 12:42:50 +0300

    change port

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3000);
+  await app.listen(8080);
   // somewhere in your initialization file
 
   app.use(
