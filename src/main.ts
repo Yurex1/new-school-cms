cc70469edff5bf9ab18285e646d22db857e98ae2
Yurex1 <popovych.yuriy2@gmail.com>
2024-12-14 00:41:39 +0200

    change express-session cookies to cookie-session

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as session from 'express-session';
+import * as session from 'cookie-session';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 
